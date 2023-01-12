27d624658b950d49ffdc0093149ed0de7d448b26
huahua <moflowerlkh@gmail.com>
2023-01-13 00:23:56 +0800

    add fbs in spark

--- examples/src/main/scala/org/apache/spark/examples/sql/kaihua/MyTest.scala
+++ examples/src/main/scala/org/apache/spark/examples/sql/kaihua/MyTest.scala
@@ -2,11 +2,13 @@ package org.apache.spark.examples.sql.kaihua
 
 import org.apache.spark.sql.SparkSession
 
-/**
- * @author kahua.li 
- * @email moflowerlkh@gmail.com
- * @date 2022/12/30
- **/
+/** @author
+  *   kahua.li
+  * @email
+  *   moflowerlkh@gmail.com
+  * @date
+  *   2022/12/30
+  */
 
 object MyTest {
   case class People(name: String, age: Int)
@@ -15,12 +17,16 @@ object MyTest {
 
   def main(args: Array[String]): Unit = {
     val spark = SparkSession
-        .builder()
-        .appName("Spark SQL basic example")
-        .master("local")
-        .config("spark.sql.codegen.wholeStage", "false")
-        .config("spark.shuffle.sort.bypassMergeThreshold", "0") // disable bypass merge sort, default is 200
-        .getOrCreate()
+      .builder()
+      .appName("Spark SQL basic example")
+      .master("local")
+      .config("oblivious.enable", "true")
+      .config("spark.sql.codegen.wholeStage", "false")
+      .config(
+        "spark.shuffle.sort.bypassMergeThreshold",
+        "0"
+      ) // disable bypass merge sort, default is 200
+      .getOrCreate()
     import spark.implicits._
     val peopleDS = Seq(
       People("LiHua", 22),
@@ -28,17 +34,20 @@ object MyTest {
     ).toDS()
     val carDS = Seq(
       Car(101, "LiHua"),
-      Car(102, "XYue"),
+      Car(102, "XYue")
     ).toDS()
     peopleDS.createTempView("people")
     carDS.createTempView("car")
 
     if (1 == 1) {
-      spark.sql("select  /*+ SHUFFLE_HASH(people) */ * " +
-          "from people join car on people.name = car.owner")
-          .show()
+      spark
+        .sql(
+          "select  /*+ SHUFFLE_HASH(people) */ * " +
+            "from people join car on people.name = car.owner"
+        )
+        .show()
     } else {
-      spark.sql("select avg(age) from people").show() 
+      spark.sql("select avg(age) from people").show()
     }
     Thread.sleep(60 * 60 * 1000)
   }
